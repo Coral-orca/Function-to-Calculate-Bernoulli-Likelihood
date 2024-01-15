8c5660e735f71dbe82f6ab1ef7a56942e186f270
Nilabhra Rohan Das <149663200+Coral-orca@users.noreply.github.com>
2024-01-15 21:44:02 +0000

    Update bernoulli-likelihood.R

--- bernoulli-likelihood.R
+++ bernoulli-likelihood.R
@@ -1,23 +1,23 @@
-# Formulating a function for bernoulli lielihood
-# Plotting likelihoods for bernoulli distribution
-# Define a function to compute likelihood
+# Formulating a function for Bernoulli likelihood
+# Plotting likelihoods for Bernoulli distribution
+# Define a function to compute the likelihood
 likelihood = function(n, y, theta) {
   return(theta^y * (1 - theta)^(n-y))
 }
 
 # Create a sequence of values of mortality rates
-theta = seq(0.01, 0.99, 0.01)
+theta = seq(0.01, 0.99, 0.01) # This simulates a hypothetical situation
 
 # Plot the above data and function
 plot(theta, likelihood(500, 60, theta))
 abline(v = (60/500)) # Likelihood is maximised at this point
 
-# Define a function to compute log likelihood
-# Log likelihood is much easier and stable to interpret
+# Define a function to compute log-likelihood
+# Log likelihood is much easier and more stable to interpret
 loglike = function(n, y, theta) {
   return(y * log(theta) + (n-y) * log(1 - theta))
 }
 
-# Plot the sequence against the log likelihood of that sequence
+# Plot the sequence against the log-likelihood of that sequence
 plot(theta, loglike(500, 60, theta), type = "l")
 abline(v = (60/500))
